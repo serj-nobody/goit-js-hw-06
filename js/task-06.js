6333f0cae6a7616558b74c8eb64471e0c0ef8599
Serj <mrnobody959@gmail.com>
2022-05-16 15:20:30 +0300

    update task 6

--- js/task-06.js
+++ js/task-06.js
@@ -1,39 +1,18 @@
-const inputRef = document.querySelector('input');
-const dataLength = inputRef.getAttribute('data-length');
+const refs = {
+  dataInput: document.querySelector('input'),
+  dataLength: document.querySelector('input').getAttribute('data-length'),
+}
 
 function onInputBlur() {
-  if (inputRef.value.length !== dataLength) {
-    inputRef.classList.remove('valid');
-    inputRef.classList.add('invalid');
+  if (refs.dataInput.value.length !== refs.dataLength) {
+    refs.dataInput.classList.remove('valid');
+    refs.dataInput.classList.add('invalid');
   }
-  if (inputRef.value.length == dataLength) {
-    inputRef.classList.remove('invalid');
-    inputRef.classList.add('valid');
+  if (refs.dataInput.value.length == refs.dataLength) {
+    refs.dataInput.classList.remove('invalid');
+    refs.dataInput.classList.add('valid');
   }
 }
 
-inputRef.addEventListener("blur", onInputBlur);
-
-
-
-
-// ---- не можу зрозуміти чому такий варіант коду нижче не працює? ------
-
-// const refs = {
-//   dataInput: document.querySelector('input'),
-//   dataLength: dataInput.getAttribute('data-length'),
-// }
-
-// function validateInputHandler() {
-//   if (refs.dataInput.value.length !== refs.dataLength) {
-//     refs.dataInput.classList.remove('valid');
-//     refs.dataInput.classList.add('invalid');
-//   }
-//   if (refs.dataInput.value.length == refs.dataLength) {
-//     refs.dataInput.classList.remove('invalid');
-//     refs.dataInput.classList.add('valid');
-//   }
-// }
-
-// refs.dataInput.addEventListener("blur", validateInputHandler);
+refs.dataInput.addEventListener("blur", onInputBlur);
 
